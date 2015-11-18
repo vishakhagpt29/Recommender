811906a79714d426a59c44a340523410c135e626
Vishakha <vishakhagpt29@github.com>
2015-11-18 20:49:00 +0530

    metadata for all databases

---
+++ data.py
@@ -0,0 +1,16 @@
+
+"""
+##DATABASES##
+critics database: nested dictionary, users and their movie lists and ratings
+year: dictionary, movie name as key and year as value
+category: dictionary, movie name as key and a list containing one or more category
+movies: list, list of movies
+year_movie: dictionary, year as key and list of movies in released in that year as value
+genre_movie: dictionary, genre as key and list of movies belonging to that genre
+average_rating: dictionary, movie as key and their average rating as value
+names: list of 100 unique names
+"""
+
+
+
+
