09a273f718d3ec5e86a76e38af300b9a46734de8
Vishakha <vishakhagpt29@github.com>
2015-11-18 20:49:35 +0530

    Create critics dictionary

---
+++ gen_critics.py
@@ -0,0 +1,20 @@
+from data import movies
+from data import names
+import random
+
+#assigns random rating for movies of user, generates critics nested dictionary
+def gen_critics(critics):
+    sub_dict = {}
+    for name in names:
+        no_of_movies = random.randrange(3,10)
+        while(no_of_movies > 0):
+            sub_dict[data.movies[random.randrange(0,len(movies))]] = random.randrange(0,5)
+            no_of_movies = no_of_movies - 1
+        critics[name] = sub_dict
+        sub_dict = {}
+    return critics
+
+if __name__ == "__main__":
+    critics = {}
+    critics = gen_critics(critics)
+    print critics
