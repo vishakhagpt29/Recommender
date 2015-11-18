386a358ec7a11bb5c1ed566479222eee4e2ed7b7
Vishakha <vishakhagpt29@github.com>
2015-11-18 20:52:14 +0530

    Genre list

---
+++ genre.py
@@ -0,0 +1,21 @@
+genre = {
+'Action',
+'Adventure',
+'Animation',
+'Children\'s',
+'Comedy',
+'Crime',
+'Documentary',
+'Drama',
+'Fantasy',
+'Film-Noir',
+'Horror',
+'Musical',
+'Mystery',
+'Romance',
+'Sci-Fi',
+'Thriller',
+'War',
+'Western'
+}
+
